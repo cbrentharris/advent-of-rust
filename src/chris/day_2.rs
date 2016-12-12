9ba9566ae810a67904a2353331316ab59e87b153
Christopher Harris <chharris@linkedin.com>
2016-12-12 09:32:49 -0600

    Day 2 wip

--- src/chris/day_2.rs
+++ src/chris/day_2.rs
@@ -12,6 +12,60 @@ pub fn char_to_keypad_direction(keypad_char: char) -> KeypadDirection {
         'D' => KeypadDirection::Down,
         'L' => KeypadDirection::Left,
         'R' => KeypadDirection::Right,
-        _ => panic!("Invalid turn string: {}. Expected 'U', 'D', 'L', or 'R'", keypad_char)
+        _ => panic!("Invalid keypad char: {}. Expected 'U', 'D', 'L', or 'R'", keypad_char)
     }
+}
+
+pub fn row_col_to_keypad_button(row: i32, col: i32) -> char {
+    match (row, col) {
+        (0, 0) => '1',
+        (0, 1) => '2',
+        (0, 2) => '3',
+        (1, 0) => '4',
+        (1, 1) => '5',
+        (1, 2) => '6',
+        (2, 0) => '7',
+        (2, 1) => '8',
+        (2, 2) => '9',
+        (_, _) => panic!("Whoops!")
+    }
+}
+
+pub fn day_2(input: String) {
+    let mut row = 1;
+    let mut col = 1;
+    let MAX_ROW = 2;
+    let MAX_COL = 2;
+    let MIN_ROW = 0;
+    let MIN_COL = 0;
+
+    let split = input.split_whitespace().collect::<Vec<&str>>();
+    for sequence in &split {
+        for (i, c) in sequence.chars().enumerate() {
+            let keypad_direction = char_to_keypad_direction(c);
+            match keypad_direction {
+                KeypadDirection::Up => {
+                    if (row > MIN_ROW) {
+                        row -= 1;
+                    }
+                },
+                KeypadDirection::Down => {
+                    if (row < MAX_ROW) {
+                        row += 1;
+                    }
+                },
+                KeypadDirection::Left => {
+                    if (col > MIN_COL) {
+                        col -= 1;
+                    }
+                },
+                KeypadDirection::Right => {
+                    if (col < MAX_COL) {
+                        col += 1;
+                    }
+                }
+            }
+        }
+    }
+    row_col_to_keypad_button(row, col);
 }
--- tests/chris_day_2_tests.rs
+++ tests/chris_day_2_tests.rs
@@ -1,5 +1,5 @@
 extern crate advent_lib;
-use advent_lib::chris::day_2::{KeypadDirection, char_to_keypad_direction};
+use advent_lib::chris::day_2::{KeypadDirection, char_to_keypad_direction, day_2};
 use advent_lib::input_reader;
 
 #[test]
@@ -8,4 +8,13 @@ fn keypad_char_to_direction() {
     assert_eq!(char_to_keypad_direction('R'), KeypadDirection::Right);
     assert_eq!(char_to_keypad_direction('U'), KeypadDirection::Up);
     assert_eq!(char_to_keypad_direction('D'), KeypadDirection::Down);
+}
+
+#[test]
+fn day_1_test() {
+    let mut s = String::new();
+    input_reader::read_file("src/resources/day2/chris_input.txt", &mut s);
+    day_2(s);
+    // Uncomment to see output of day_2
+    // assert_eq!(true, false);
 }
