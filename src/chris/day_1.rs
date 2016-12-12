3967498aea3748467c21839d6cbdf38b0784455a
Christopher Harris <chharris@linkedin.com>
2016-12-12 15:38:59 -0600

    Day 1 part 2

--- src/chris/day_1.rs
+++ src/chris/day_1.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::collections::HashSet;
 
 #[derive(PartialEq, Debug)]
 pub enum Direction {
@@ -39,11 +39,28 @@ pub fn string_to_turn_direction(turn_str: char) -> TurnDirection {
   }
 }
 
+pub fn check_point(x: i32, y: i32, set: &mut HashSet<(i32, i32)>) -> bool {
+  println!("Checking {}, {}", x.to_string(), y.to_string());
+  if set.contains(&(x, y)) {
+    println!("First duplicate coordinate: {}, {}. It is {} blocks away.",
+             x.to_string(),
+             y.to_string(),
+             (x.abs() + y.abs()).to_string()
+    );
+    return true;
+  } else {
+    set.insert((x, y));
+    return false;
+  }
+}
+
 pub fn day_1(input: String) {
   let mut x = 0;
   let mut y = 0;
   let split = input.split(", ").collect::<Vec<&str>>();
   let mut current_direction = Direction::North;
+  let mut coord_set: HashSet<(i32, i32)> = HashSet::new();
+  let mut found_visited_point = false;
   for direction in &split {
     let direction_string = String::from(*direction);
     let mut direction_chars = direction_string.chars();
@@ -53,11 +70,40 @@ pub fn day_1(input: String) {
         .parse::<i32>().unwrap();
     current_direction = direction_now_facing(turn_direction, current_direction);
     match current_direction {
-      Direction::North => y += num_steps,
-      Direction::South => y -= num_steps,
-      Direction::East => x -= num_steps,
-      Direction::West => x += num_steps
+      Direction::North => {
+        for i in y..y + num_steps {
+          if !found_visited_point {
+            found_visited_point = check_point(x, i, &mut coord_set);
+          }
+        }
+        y += num_steps;
+      },
+      Direction::South => {
+        for i in (y - (num_steps - 1)..y + 1).rev() {
+          if !found_visited_point {
+            found_visited_point = check_point(x, i, &mut coord_set);
+          }
+        }
+        y -= num_steps
+      },
+      Direction::East =>{
+        for i in x..x + num_steps {
+          if !found_visited_point {
+            found_visited_point = check_point(i, y, &mut coord_set);
+          }
+        }
+        x += num_steps
+      },
+      Direction::West => {
+        for i in (x - (num_steps - 1)..x + 1).rev() {
+          if !found_visited_point {
+            found_visited_point = check_point(i, y, &mut coord_set);
+          }
+        }
+        x -= num_steps
+      }
     }
   }
   println!("{}", (y.abs() + x.abs()).to_string());
 }
+
