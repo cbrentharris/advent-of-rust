a73d0f88e755e5ee20ec87c9173b6fbfbc41696c
Christopher Harris <chharris@linkedin.com>
2016-12-13 08:55:11 -0600

    Day 3 part 2 chris

--- src/chris/day_3.rs
+++ src/chris/day_3.rs
@@ -1,17 +1,28 @@
 pub fn day_3(input: String) {
     let split = input.split("\n").collect::<Vec<&str>>();
     let mut possible_triangles = 0;
-    for triangle in &split {
-        let sides = triangle.split_whitespace().collect::<Vec<&str>>();
-        let a = sides[0].parse::<i32>().unwrap();
-        let b = sides[1].parse::<i32>().unwrap();
-        let c = sides[2].parse::<i32>().unwrap();
-        let a_is_possible = a < b + c;
-        let b_is_possible = b < a + c;
-        let c_is_possible = c < a + b;
-        if a_is_possible && b_is_possible && c_is_possible {
-            possible_triangles += 1;
+    let mut i = 0;
+    while i < split.len() {
+        let row_1 = split[i];
+        let row_2 = split[i + 1];
+        let row_3 = split[i + 2];
+
+        let a_s = row_1.split_whitespace().collect::<Vec<&str>>();
+        let b_s = row_2.split_whitespace().collect::<Vec<&str>>();
+        let c_s = row_3.split_whitespace().collect::<Vec<&str>>();
+
+        for j in 0..3 {
+            let a = a_s[j].parse::<i32>().unwrap();
+            let b = b_s[j].parse::<i32>().unwrap();
+            let c = c_s[j].parse::<i32>().unwrap();
+            let a_is_possible = a < b + c;
+            let b_is_possible = b < a + c;
+            let c_is_possible = c < a + b;
+            if a_is_possible && b_is_possible && c_is_possible {
+                possible_triangles += 1;
+            }
         }
+        i += 3;
     }
     println!("{}", possible_triangles.to_string());
 }
