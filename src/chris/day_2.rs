43b01cc5e340468787331daffc526f3027d98170
Christopher Harris <chharris@linkedin.com>
2016-12-12 22:12:03 -0600

    Day 2 part 2 -- chris

--- src/chris/day_2.rs
+++ src/chris/day_2.rs
@@ -16,26 +16,60 @@ pub fn char_to_keypad_direction(keypad_char: char) -> KeypadDirection {
     }
 }
 
+/**
+   1
+  2 3 4
+5 6 7 8 9
+  A B C
+    D
+*/
 pub fn row_col_to_keypad_button(row: i32, col: i32) -> char {
     match (row, col) {
-        (0, 0) => '1',
-        (0, 1) => '2',
-        (0, 2) => '3',
-        (1, 0) => '4',
-        (1, 1) => '5',
-        (1, 2) => '6',
-        (2, 0) => '7',
-        (2, 1) => '8',
-        (2, 2) => '9',
+        (0, 2) => '1',
+        (1, 1) => '2',
+        (1, 2) => '3',
+        (1, 3) => '4',
+        (2, 0) => '5',
+        (2, 1) => '6',
+        (2, 2) => '7',
+        (2, 3) => '8',
+        (2, 4) => '9',
+        (3, 1) => 'A',
+        (3, 2) => 'B',
+        (3, 3) => 'C',
+        (4, 2) => 'D',
         (_, _) => panic!("Whoops!")
     }
 }
 
+/**
+* This function says if you can move up on a diagonal based keypad based on your current row and col
+*/
+pub fn can_move_up(row: i32, col: i32, max_row: i32, max_col: i32) -> bool {
+    let mid = (max_row / 2 - col).abs();
+    return row - mid > 0;
+}
+
+pub fn can_move_down(row: i32, col: i32, max_row: i32, max_col: i32) -> bool {
+    let mid = (max_row / 2 - col).abs();
+    return row + mid < max_row;
+}
+
+pub fn can_move_left(row: i32, col: i32, max_row: i32, max_col: i32) -> bool {
+    let mid = (max_row / 2 - row).abs();
+    return col - mid > 0;
+}
+
+pub fn can_move_right(row: i32, col: i32, max_row: i32, max_col: i32) -> bool {
+    let mid = (max_row / 2 - row).abs();
+    return col + mid < max_col;
+}
+
 pub fn day_2(input: String) {
-    let mut row = 1;
-    let mut col = 1;
-    let MAX_ROW = 2;
-    let MAX_COL = 2;
+    let mut row = 3;
+    let mut col = 0;
+    let MAX_ROW = 4;
+    let MAX_COL = 4;
     let MIN_ROW = 0;
     let MIN_COL = 0;
 
@@ -45,22 +79,22 @@ pub fn day_2(input: String) {
             let keypad_direction = char_to_keypad_direction(c);
             match keypad_direction {
                 KeypadDirection::Up => {
-                    if (row > MIN_ROW) {
+                    if can_move_up(row, col, MAX_ROW, MAX_COL) {
                         row -= 1;
                     }
                 },
                 KeypadDirection::Down => {
-                    if (row < MAX_ROW) {
+                    if can_move_down(row, col, MAX_ROW, MAX_COL) {
                         row += 1;
                     }
                 },
                 KeypadDirection::Left => {
-                    if (col > MIN_COL) {
+                    if can_move_left(row, col, MAX_ROW, MAX_COL) {
                         col -= 1;
                     }
                 },
                 KeypadDirection::Right => {
-                    if (col < MAX_COL) {
+                    if can_move_right(row, col, MAX_ROW, MAX_COL) {
                         col += 1;
                     }
                 }
--- tests/chris_day_2_tests.rs
+++ tests/chris_day_2_tests.rs
@@ -1,5 +1,5 @@
 extern crate advent_lib;
-use advent_lib::chris::day_2::{KeypadDirection, char_to_keypad_direction, day_2};
+use advent_lib::chris::day_2::{KeypadDirection, char_to_keypad_direction, can_move_up, can_move_down, can_move_left, can_move_right, day_2};
 use advent_lib::input_reader;
 
 #[test]
@@ -11,7 +11,97 @@ fn keypad_char_to_direction() {
 }
 
 #[test]
-fn day_1_test() {
+fn can_move_up_test() {
+    let max_row = 4;
+    let max_col = 4;
+    assert!(!can_move_up(2, 0, max_row, max_col));
+    assert!(!can_move_up(1, 1, max_row, max_col));
+    assert!(!can_move_up(0, 2, max_row, max_col));
+    assert!(!can_move_up(1, 3, max_row, max_col));
+    assert!(!can_move_up(2, 4, max_row, max_col));
+
+
+    assert!(can_move_up(2, 1, max_row, max_col));
+    assert!(can_move_up(3, 1, max_row, max_col));
+
+    assert!(can_move_up(1, 2, max_row, max_col));
+    assert!(can_move_up(2, 2, max_row, max_col));
+    assert!(can_move_up(3, 2, max_row, max_col));
+    assert!(can_move_up(4, 2, max_row, max_col));
+
+    assert!(can_move_up(2, 3, max_row, max_col));
+    assert!(can_move_up(3, 3, max_row, max_col));
+}
+
+#[test]
+fn can_move_down_test() {
+    let max_row = 4;
+    let max_col = 4;
+    assert!(!can_move_down(2, 0, max_row, max_col));
+    assert!(!can_move_down(3, 1, max_row, max_col));
+    assert!(!can_move_down(4, 2, max_row, max_col));
+    assert!(!can_move_down(3, 3, max_row, max_col));
+    assert!(!can_move_down(2, 4, max_row, max_col));
+
+
+    assert!(can_move_down(1, 1, max_row, max_col));
+    assert!(can_move_down(2, 1, max_row, max_col));
+
+    assert!(can_move_down(0, 2, max_row, max_col));
+    assert!(can_move_down(1, 2, max_row, max_col));
+    assert!(can_move_down(2, 2, max_row, max_col));
+    assert!(can_move_down(3, 2, max_row, max_col));
+
+    assert!(can_move_down(1, 3, max_row, max_col));
+    assert!(can_move_down(2, 3, max_row, max_col));
+}
+
+#[test]
+fn can_move_left_test() {
+    let max_row = 4;
+    let max_col= 4;
+    assert!(!can_move_left(2, 0, max_row, max_col));
+    assert!(!can_move_left(1, 1, max_row, max_col));
+    assert!(!can_move_left(0, 2, max_row, max_col));
+    assert!(!can_move_left(3, 1, max_row, max_col));
+    assert!(!can_move_left(4, 2, max_row, max_col));
+
+    assert!(can_move_left(1, 2, max_row, max_col));
+    assert!(can_move_left(1, 3, max_row, max_col));
+
+    assert!(can_move_left(2, 1, max_row, max_col));
+    assert!(can_move_left(2, 2, max_row, max_col));
+    assert!(can_move_left(2, 3, max_row, max_col));
+    assert!(can_move_left(2, 4, max_row, max_col));
+
+    assert!(can_move_left(3, 2, max_row, max_col));
+    assert!(can_move_left(3, 3, max_row, max_col));
+}
+
+#[test]
+fn can_move_right_test() {
+    let max_row = 4;
+    let max_col= 4;
+    assert!(!can_move_right(0, 2, max_row, max_col));
+    assert!(!can_move_right(1, 3, max_row, max_col));
+    assert!(!can_move_right(2, 4, max_row, max_col));
+    assert!(!can_move_right(3, 3, max_row, max_col));
+    assert!(!can_move_right(4, 2, max_row, max_col));
+
+    assert!(can_move_right(1, 1, max_row, max_col));
+    assert!(can_move_right(1, 2, max_row, max_col));
+
+    assert!(can_move_right(2, 0, max_row, max_col));
+    assert!(can_move_right(2, 1, max_row, max_col));
+    assert!(can_move_right(2, 2, max_row, max_col));
+    assert!(can_move_right(2, 3, max_row, max_col));
+
+    assert!(can_move_right(3, 1, max_row, max_col));
+    assert!(can_move_right(3, 2, max_row, max_col));
+}
+
+#[test]
+fn day_2_test() {
     let mut s = String::new();
     input_reader::read_file("src/resources/day2/chris_input.txt", &mut s);
     day_2(s);
